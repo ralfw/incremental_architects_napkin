0a00823a3fbeaf88208de756c5cc81e59e6d2fff
Ralf Westphal <info@ralfw.de>
2014-08-23 19:57:35 +0200

    #5 sample 2 - wordwrap - walking skeleton

---
+++ deduplicate/wordwrap/Formatter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wordwrap
+{
+    public class Formatter
+    {
+        public string WordWrap(string text, int maxLineLength)
+        {
+            var words = Extract_words(text);
+            var lines = Reformat(words, maxLineLength);
+            return Assemble_text(lines);
+        }
+
+        private string[] Extract_words(string text)
+        {
+            return text.Split(new[] {' '}, 
+                              StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private string[] Reformat(string[] words, int maxLineLength)
+        {
+            return words;
+        }
+
+        private string Assemble_text(string[] lines)
+        {
+            return string.Join("\n", lines);
+        }
+    }
+}
---
+++ deduplicate/wordwrap/test_Formatter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace wordwrap
+{
+    [TestFixture]
+    public class test_Formatter
+    {
+        [Test]
+        public void Acceptance()
+        {
+            var sut = new Formatter();
+            var result = sut.WordWrap("the quick brown fox jumps over the lazy dog", 9);
+            Console.WriteLine(result);
+        }
+    }
+}
