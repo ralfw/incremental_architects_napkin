820ca67a9ec6395e2c6c96d24fb2d3061cfdfe0e
Ralf Westphal <info@ralfw.de>
2014-09-13 14:03:06 +0200

    Added solution consisting of just one Operation

--- scratchpad/csvformatting/CsvFormatter.cs
+++ scratchpad/csvformatting/CsvFormatter.cs
@@ -46,6 +46,20 @@ namespace csvformatting
 		string Build_table(string[] formattedRecords, string separator) {
 			return string.Join ("\n", new[]{ formattedRecords [0], separator }.Concat(formattedRecords.Skip (1)));
 		}
+
+
+
+		public string Format_as_pure_Operation(string csvText) {
+			var csvLines = csvText.Split('\n');
+			var csvRecords = csvLines.Select (l => l.Split (';')).ToArray ();
+			var colWidths = new int[csvRecords.First ().Length];
+			for (var iCol = 0; iCol < colWidths.Length; iCol++)
+				colWidths [iCol] = csvRecords.Max (r => r [iCol].Length);
+			var formattedRecords = csvRecords.Select (r => string.Join ("|", r.Select((v, i) => v.PadRight(colWidths[i]))))
+				.ToArray ();
+			var separator = string.Join ("+", colWidths.Select (w => new string ('-', w)));
+			return string.Join ("\n", new[]{ formattedRecords [0], separator }.Concat(formattedRecords.Skip (1)));
+		}
 	}
 }
 
